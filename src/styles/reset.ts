e3a53da768e3b3fd9dede70ba6a03a2eab5fb654
Miquel Xarau <miqxarau@gmail.com>
2019-06-03 19:26:08 +0200

    updated

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -6,8 +6,18 @@ const reset = css`
 //   background-color: rgba(154, 154, 154, 1) !important;
 //   height:10px !important;
 // }
+#___gatsby > div > div > main > div.elements__Box-sc-1dburgb-0.project__Content-sc-46qbt-1.kIsAuw > div{
+  grid-gap: 3em;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: transparent;
+  grid-row-gap: inherit;
+  @media screen and (max-width: 650px) {
+    grid-template-columns: 1fr;
+  }
+}
 
-#___gatsby > div > div > main > div.elements__AnimatedBox-sc-1dburgb-1.project__PBox-sc-46qbt-0.jYDCVn {
+#___gatsby > div > div > main > div.elements__Box-cOJFaO.project__Content-kNqxAg.BRdya > div {
   grid-gap: 3em;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
